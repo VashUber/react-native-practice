58a79f5e2c52fc6cc6c1dd35a58670da31efd23c
VashUber <mrjarokvk@gmail.com>
2024-03-05 20:41:07 +0300

    fix: task5

--- sem5/task5/components/common/cards-container/CardsContainer.jsx
+++ sem5/task5/components/common/cards-container/CardsContainer.jsx
@@ -9,33 +9,22 @@ export const CardsContainer = ({ data }) => {
     return width > 720 ? 2 : 1;
   }, [width]);
 
-  // такой вот хак для numColumns
-  return numColumns === 1 ? (
+  return (
     <FlatList
-      key={1}
+      // такой вот хак для numColumns
+      key={numColumns}
       style={{
         flex: 1, // шорткат для flex-grow, flex-shrink, flex-basis, так как указано одно значение, устанавливает значение flex-grow: 1, растягивает элемент на всю ширину и высоту родителя
       }}
       data={data}
-      renderItem={({ item }) => <Card style={{ width: "100%" }} info={item} />}
+      renderItem={({ item }) => (
+        <Card
+          style={{ width: numColumns === 1 ? "100%" : "50%" }}
+          info={item}
+        />
+      )}
       keyExtractor={(e) => e.id}
-      numColumns={1}
-      contentContainerStyle={{
-        paddingHorizontal: 10, // внутренний горизонтальный отступ
-        gap: 20, // расстояние между элементами,
-        alignItems: "center", //управляет расположением по второстепенной оси, в этом случае по центру
-      }}
-    />
-  ) : (
-    <FlatList
-      key={2}
-      style={{
-        flex: 1, // шорткат для flex-grow, flex-shrink, flex-basis, так как указано одно значение, устанавливает значение flex-grow: 1, растягивает элемент на всю ширину и высоту родителя
-      }}
-      data={data}
-      renderItem={({ item }) => <Card style={{ width: "50%" }} info={item} />}
-      keyExtractor={(e) => e.id}
-      numColumns={2}
+      numColumns={numColumns}
       contentContainerStyle={{
         paddingHorizontal: 10, // внутренний горизонтальный отступ
         gap: 20, // расстояние между элементами,
